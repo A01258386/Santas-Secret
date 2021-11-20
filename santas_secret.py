0af168d35b826f99baeb0a49e81104fe7e5ea6df
Gokce Gokmen <51439620+A01258386@users.noreply.github.com>
2021-11-19 21:01:24 -0800

    Santas Secret Program

---
+++ santas_secret.py
@@ -0,0 +1,21 @@
+#Santa's Secret
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+import random,time
+
+app = Flask(__name__)
+@app.route('/',methods=['GET','POST'])
+
+
+def santas_secret():
+    transcript = ""
+    friends = ['Amanda', 'Alex A', 'Alex H', 'Gokce','Steven','Yi']
+    random.shuffle(friends)
+    
+    if request.method == "POST":
+        print("FORM DATA RECEIVED")
+        transcript = random.choice(friends)
+    # print('Now I will tell you who is your secret friend!')
+    return render_template('index.html', transcript=transcript)
+
+if __name__ == '__main__':
+    app.run(debug=True,threaded=True)
